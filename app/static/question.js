228cb96b58fe21d2d4c3eb9043add2860ceb0cfc
liukang451 <64184818+liukang451@users.noreply.github.com>
2020-05-18 21:07:35 +0800

    quiz javascript

---
+++ app/static/question.js
@@ -0,0 +1,85 @@
+var questions = [{
+	"question": "How is COVID-19 passed on?",
+	"option1": "Through droplets that come from your mouth and nose when you cough or breathe out",
+	"option2": "In sexual fluids, including semen, vaginal fluids or anal mucous",
+	"option3": "By drinking unclean water",
+	"option4": "All ablove",
+	"answer": "1"
+}, {
+	"question": "What are the common symptoms of COVID-19?",
+	"option1": "A new and continuous cough",
+	"option2": "Fever",
+	"option3": "Tiredness",
+	"option4": "All of the above",
+	"answer": "1"
+}, {
+	"question": "Which of the following group is COVID-19 most at risk?",
+	"option1": "Children",
+	"option2": "Older people, especially those aged 70 and above",
+	"option3": "People with certain underlying health conditions",
+	"option4": "Australian people",
+	"answer": "2"
+}, {
+	"question": "How does Corona Virus spread?",
+	"option1": "Animal to person",
+	"option2": "Person to person",
+	"option3": " Corona beer to person",
+	"option4": "Food to person",
+	"answer": "2"
+}, {
+	"question": "Can washing your hands frequently protect you from COVID-19?",
+	"option1": "Yes, but only is you use a strong bleach",
+	"option2": "Yes, normal soap and water or hand sanitizer is enough",
+	"option3": "No, washing your hands does not stop COVID-19",
+	"option4": "No, but using Vodka wash hand can",
+	"answer": "2"
+}, {
+	"question": "Can wear a mask protect you from COVID-19 when you going outside?",
+	"option1": "Yes, wear a mask at all times to prevent COVID -19",
+	"option2": "No, You don't need to wear a mask if you are healthy and not caring for a sick person",
+	"option3": "No, wearing a mask is a violation of human right",
+	"option4": " Yes, but only when going to hospital",
+	"answer": "1"
+}, {
+	"question": "How long do you need to wash hands with soap and water for?",
+	"option1": "10 Seconds",
+	"option2": "15 Seconds",
+	"option3": "20 Seconds",
+	"option4": "As long as you can",
+	"answer": "3"
+}, {
+	"question": "What is the least percentage of alcohol that a hand sanitizer should contains?",
+	"option1": "30%",
+	"option2": "40%",
+	"option3": "60%",
+	"option4": "99%",
+	"answer": "3"
+}, {
+	"question": "Which of the following is not an effective disinfectant for killing coronavirus?",
+	"option1": "Alcohol",
+	"option2": "Water",
+	"option3": "Bleach",
+	"option4": "Ammonia",
+	"answer": "2"
+}, {
+	"question": "Can  you always tell if someone has COVID-19?",
+	"option1": "NO, not everyone with COVID-19 has symptoms",
+	"option2": "Yes, it always obvious since a person with COVID-19 coughs a lot",
+	"option3": "Yes, you can just by where a person coms from, their race and ethnicity",
+	"option4": "Control Unit",
+	"answer": "1"
+}, {
+	"question": "What's the safety social distance between each person?",
+	"option1": "0.9m",
+	"option2": "1.2m",
+	"option3": "1.5m",
+	"option4": "2.0m",
+	"answer": "3"
+}, {
+	"question": "Select the improper method when you dining at public enviroment?",
+	"option1": "Washing hands before having meal",
+	"option2": "Using disposable glove to handle tableware",
+	"option3": "Stay a proper social distance with others",
+	"option4": "Using hand to handle food without any sterilize",
+	"answer": "4"
+}];

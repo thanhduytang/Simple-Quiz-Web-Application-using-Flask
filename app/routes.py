41f54278aa7902d698599831de96828731a87aa1
thanhduytang <62139045+thanhduytang@users.noreply.github.com>
2020-05-20 20:15:45 +0800

    Update function editquestion

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template, flash, redirect, url_for, request, json
+from flask import render_template, flash, redirect, url_for, request, abort
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, QuizForm
+from app.forms import LoginForm, RegistrationForm, QuizForm, EditForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Score, QA
 from werkzeug.urls import url_parse
@@ -68,6 +68,34 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+@app.route('/editquestion', methods=['GET', 'POST'])
+@login_required
+def editquestion():
+    if not current_user.is_admin:
+        flash("You are not allowed to access this page. Administrator only !!!")
+        return redirect(url_for('index'))
+    
+    questions = QA.query.all()
+    edit_form = EditForm()
+    if edit_form.validate_on_submit():
+        flash("You have successfully created a question")
+        if QA.query.get(edit_form.question_num.data) is not None:
+            db.session.delete(QA.query.get(edit_form.question_num.data))
+            db.session.commit()
+        newquestion = QA(id      = edit_form.question_num.data,
+                         question= edit_form.question.data,
+                         option1 = edit_form.option1.data,
+                         option2 = edit_form.option2.data,
+                         option3 = edit_form.option3.data,
+                         answer  = edit_form.answer.data)
+        db.session.add(newquestion)
+        db.session.commit()
+        return render_template('edit.html', form=edit_form, questions=questions)
+    return render_template('edit.html', form=edit_form, questions=questions)
+
+
+
+
 @app.route('/quiz', methods=['GET', 'POST'])
 @login_required
 def quiz():
@@ -76,9 +104,21 @@ def quiz():
     pullquest1 = pullquest[0:4]
     pullquest2 = pullquest[4:9]
     pullquest3 = pullquest[9:13]
-    return render_template('quiz.html', form = form, pullquest1 =pullquest1, pullquest2=pullquest2, pullquest3 =pullquest3)
+    if form.validate_on_submit():
+        return redirect(url_for('info'))
+    else:
+        return render_template('quiz.html', form = form, pullquest1 =pullquest1, pullquest2=pullquest2, pullquest3 =pullquest3)
 
-@app.route('/result', methods=['GET', 'POST'])
+
+@app.route('/result', methods=['POST'])
 @login_required
-def result():
-    return render_template('result.html')
+def info():
+    score=0
+    choices  = QA.query.filter(QA.id).all()
+    for i in range(13):
+        answered = request.args.get('value', i, type=str)
+        if choices[i].answer == answered:
+            score += 10
+            return render_template('result.html', score=score)
+        else:
+            return abort(500)
